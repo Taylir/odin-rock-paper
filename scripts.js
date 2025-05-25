892e33952be5de00af3dd76ca9c12d0fd78682a3
Taylir <tdl9716@gmail.com>
2025-05-25 16:41:05 -0400

    Add robot decision and broadcast to user

--- scripts.js
+++ scripts.js
@@ -18,16 +18,32 @@ function askForChoice(question) {
 
 async function getUserChoice() {
   let option = "";
-  let count = 0;
-  while ( option !== "r" && option !== "p" && option !== "s") {
-    option = await askForChoice("Rock, Paper, Scissors: ");
+  while (option !== "r" && option !== "p" && option !== "s") {
+    option = await askForChoice("(R)ock, (P)aper, (S)cissors: ");
     option = option.toLowerCase().slice(0, 1);
-    ++count;
-    console.log(option);
-    console.log(count);
   }
   console.log(`You Picked ${option}`);
   rl.close();
+  return option;
 }
 
-getUserChoice();
+function getRobotChoice() {
+  //Pick number 1-3
+  const number = Math.floor(Math.random() * 3); // Expects 0 - 2
+  //associtate number with R, P, or S.
+  switch (number) {
+    case 0:
+      return "p";
+    case 1:
+      return "s";
+    case 2:
+      return "r";
+  }
+}
+
+async function decideWinner() {
+  console.log(`User Selected ${await getUserChoice()}`);
+  console.log(`Robot Selected ${getRobotChoice()}`);
+}
+
+decideWinner();
